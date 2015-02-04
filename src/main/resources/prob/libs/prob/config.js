dd1c4966325e2273080d37b2c2058b9a5600fa81
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-02-04 15:49:34 +0100

    shifted js libraries

--- src/main/resources/prob/libs/prob/config.js
+++ src/main/resources/prob/libs/prob/config.js
@@ -2,11 +2,20 @@ require.config({
     paths: {
         "prob": "/bms/libs/prob/prob",
         "ngProB": "/bms/libs/prob/ngProB",
-        'prob-css': "/bms/libs/prob/prob"
+        'prob-css': "/bms/libs/prob/prob",
+        "jquery-ui-css": "/bms/libs/jquery-ui/jquery-ui.min",
+        "jquery-ui-theme-css": "/bms/libs/jquery-ui/jquery-ui.theme.min",
+        "tooltipster": "/bms/libs/tooltipster/jquery.tooltipster.min",
+        "tooltipster-css": "/bms/libs/tooltipster/tooltipster",
+        "tooltipster-shadow-css": "/bms/libs/tooltipster/themes/tooltipster-shadow",
+        "jquery-ui": "/bms/libs/jquery-ui/jquery-ui.min",
+        "jquery-cookie": "/bms/libs/jquery-cookie/jquery.cookie"
     },
     shim: {
+        'jquery-ui': ['jquery'],
+        'tooltipster': ["jquery"],
         'ngProB': ["ngBMotion", 'jquery-cookie', 'jquery-ui'],
         'prob': ['ngProB', 'bms', 'angularAMD', 'tooltipster']
     },
     deps: ['/bms/libs/bmotion/config.js']
-})
+});
