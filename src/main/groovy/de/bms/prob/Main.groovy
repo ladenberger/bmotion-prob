73cf612fe6d31948439ca5251d909ccffa11f1ae
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-02-12 22:02:33 +0100

    allow starting multiple instances of application

--- src/main/groovy/de/bms/prob/Main.groovy
+++ src/main/groovy/de/bms/prob/Main.groovy
@@ -1,10 +1,6 @@
 package de.bms.prob
 
-import com.corundumstudio.socketio.AckRequest
-import com.corundumstudio.socketio.SocketIOClient
-import com.corundumstudio.socketio.listener.DataListener
 import de.bms.server.BMotionServer
-import de.prob.webconsole.WebConsole
 
 public class Main {
 
@@ -13,16 +9,6 @@ public class Main {
         // Start BMotion Server
         BMotionServer server = ProBServerFactory.getServer(args)
         server.start()
-        if (server.standalone) {
-            server.socketServer.getServer().addEventListener("initProB", String.class, new DataListener<String>() {
-                @Override
-                public void onData(final SocketIOClient client, String str,
-                                   final AckRequest ackRequest) {
-                    if (ackRequest.isAckRequested())
-                        ackRequest.sendAckData([port: WebConsole.getPort()]);
-                }
-            });
-        }
 
         new Thread(new Runnable() {
             public void run() {
