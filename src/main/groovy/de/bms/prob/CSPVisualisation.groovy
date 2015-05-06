3ad43f5f9ea88ab1ecc80571c84f7a1fde232a22
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-05-06 10:04:46 +0200

    support for formula observer

--- src/main/groovy/de/bms/prob/CSPVisualisation.groovy
+++ src/main/groovy/de/bms/prob/CSPVisualisation.groovy
@@ -3,6 +3,7 @@ package de.bms.prob
 import de.bms.IllegalFormulaException
 import de.bms.BMotionScriptEngineProvider
 import de.prob.animator.domainobjects.IEvalElement
+import de.prob.animator.domainobjects.IdentifierNotInitialised
 import de.prob.statespace.State
 import de.prob.statespace.StateSpace
 
@@ -28,6 +29,20 @@ public class CSPVisualisation extends ProBVisualisation {
             }
         }
         return formulaCache.get(formula);
+    }
+
+    @Override
+    public Object observe(final d) {
+        def map = [:]
+        d.data.observers.each { String k, v ->
+            def result = eval(k);
+            def resString = null;
+            if (result != null && !(result instanceof IdentifierNotInitialised)) {
+                resString = result.value
+            }
+            map.put(k, [result: resString]);
+        }
+        return map
     }
 
     @Override
